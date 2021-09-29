e5110d6aba0f8513058db6ee9574f4767fdbb0fd
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-09-29 18:23:22 +0300

    3th

--- routes/counter.js
+++ routes/counter.js
@@ -1,14 +1,28 @@
 const {Router} = require('express')
 const router = Router()
-
-
+var counter = 0;
 
 router.get('/', (req, res) => {
-    res.render('index')
+
+    res.render('index', {
+        index: counter
+    })
 })
 
 router.get('/stat', (req, res) => {
-    res.render('stat')
+    ++counter
+    res.render('stat', {
+        index: counter
+    })
+   
 })
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        name: 'Artem',
+    })
+})  
+
+
 
 module.exports = router 
